1c4c653419ff538fadaa69db018c527d23776f74
Darìo <neocubo1@gmail.com>
2017-11-11 23:58:26 -0300

    Faltaba eso

---
+++ individuo.hpp
@@ -0,0 +1,35 @@
+# ifndef INDIVIDUO_HPP
+# define INDIVIDUO_HPP
+
+#include <iostream>
+#include <stdlib.h>
+#include "vector"
+
+using namespace std;
+
+class Individuo
+{
+public:
+	Individuo();
+
+	void mostrar();
+
+private:
+	vector<int> cuerpo;
+
+};
+
+Individuo::Individuo()
+{
+	for(int i = 0; i < 5; i++){
+		cuerpo.push_back(rand()%(10-1));
+	}
+}
+
+void Individuo::mostrar()
+{
+	for(int i = 0; i < 5; i++){
+		cout << cuerpo[i] << ",";
+	}
+}
+#endif
