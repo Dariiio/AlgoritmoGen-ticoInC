04d00b1dd9e66a64c0826284e22c9a83c3b1e9eb
Dario <neocubo1@gmail.com>
2017-12-30 20:56:04 +0000

    Implemento calcular_fitnes

--- individuo.hpp
+++ individuo.hpp
@@ -13,10 +13,12 @@ public:
 	Individuo();
 
 	void mostrar();
+	
+	void calcular_fitness(vector<int> argento);
 
 private:
 	vector<int> cuerpo;
-
+	int fitness ;
 };
 
 Individuo::Individuo()
@@ -24,12 +26,28 @@ Individuo::Individuo()
 	for(int i = 0; i < 5; i++){
 		cuerpo.push_back(rand()%(10-1));
 	}
+	fitness = 0;
 }
 
 void Individuo::mostrar()
 {
+	cout << "[";
 	for(int i = 0; i < 5; i++){
+		if( i == 4){
+			cout << cuerpo[i];
+		}else{
 		cout << cuerpo[i] << ",";
+		}
+	}
+	cout << "] - fitness: " << fitness;
+}
+
+void Individuo::calcular_fitness(vector<int> argento)
+{
+	for(int i = 0; i < 5; i++){
+		if( cuerpo[i] == argento[i]){
+			fitness++;
+		}
 	}
 }
 #endif
--- main.cpp
+++ main.cpp
@@ -9,8 +9,17 @@ int main(int argc, char const *argv[])
 {
 	srand((time(NULL)));
 
+	vector<int> argento;
+	
+	argento = {1,1,1,1,1};
+
 	Individuo persona1;
 
+	persona1.mostrar();
+	
+	persona1.calcular_fitness(argento);
+
+	cout << endl;
 	persona1.mostrar();
 
 	int asd;
